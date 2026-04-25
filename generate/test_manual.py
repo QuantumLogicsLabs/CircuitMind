9697c883fe07d1917661484ce0b8ffc52fb4c4e1
haseeb-arif <haseebarif8944@gmail.com>
2026-04-26 00:44:58 +0500

    feat: add generate.py and  test_manual.py with JSON output

--- generate/test_manual.py
+++ generate/test_manual.py
@@ -1,4 +1,10 @@
+import json
 from generate import generate_circuit
+
+result1 = generate_circuit("make me a LED circuit")
+result2 = generate_circuit("make me a motor circuit")
+result3 = generate_circuit("make me a buzzer circuit")
+
 
 print("Test 1: LED circuit")
 print(generate_circuit("make me a LED circuit"))
@@ -11,3 +17,8 @@ print(generate_circuit("make me a buzzer circuit"))
 
 print("Test 4: Unknown")
 print(generate_circuit("make me a robot"))
+
+with open("output.json", "w") as f:
+    json.dump({"led_circuit": result1, "motor_circuit": result2, "buzzer_circuit": result3}, f, indent=4)
+
+print("\noutput.json file created successfully!")
